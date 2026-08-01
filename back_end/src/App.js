f09345ff84a5465d3e74b46dfca822cdba9d079d
krish <coolkpatna.com>
2026-08-01 09:36:05 +0530

    fixes

--- back_end/src/App.js
+++ back_end/src/App.js
@@ -1,1 +1,40 @@
-import express from 'express'
+import cookieParser from "cookie-parser";
+
+// CORS allows frontend and backend from different origins to communicate
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(
+  cors({
+
+    // Allows requests only from this frontend URL
+    // Example:
+    // http://localhost:3000
+    origin: process.env.CORS_ORIGIN,
+
+    // Allows cookies, authorization headers, sessions etc.
+    // to be sent between frontend and backend
+    credentials: true,
+  }),
+);
+
+// Converts incoming JSON request body into JavaScript object
+app.use(
+  express.json({limit: "16kb",})
+);
+
+app.use(express.static("public"));
+
+// Reads cookies sent by browser
+// and stores them inside req.cookies
+app.use(cookieParser());
+// req.cookies.token
+
+//routs
+import  userRouter from "./routes/user.routs.js";
+
+app.use("/api/v1/users", userRouter)
+
+export default app;
